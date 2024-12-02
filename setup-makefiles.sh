LineageOS/android_device_google_taimen

taimen: extract-files improvements
Change-Id: I517698ca3b5a93e10a5b167417631c7f8d0ab9e2

setup-makefiles.sh
@@ -30,7 +30,7 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 # Warning headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}/lineage-proprietary-files.txt" true
+write_makefiles "${MY_DIR}/lineage-proprietary-files.txt"
 write_makefiles "${MY_DIR}/lineage-proprietary-files-vendor.txt" true
 
 append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"

arprojectstudio/device_google_taimen

setup-makefiles.sh
@@ -30,8 +30,8 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 # Warning headers and guards
 write_headers
 
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-write_makefiles "${MY_DIR}/proprietary-files-vendor.txt" true
+write_makefiles "${MY_DIR}/proprietary-files.txt"
+write_makefiles "${MY_DIR}/proprietary-files-vendor.txt"
 
 append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
 